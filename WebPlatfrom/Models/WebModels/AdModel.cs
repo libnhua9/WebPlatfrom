3ec8ccc5eae1be15ce3b610ffa93fe973a02a8cd
LBH <LBH@DESKTOP-S6EVH7C>
2017-06-03 19:18:01 +0800

    添加admodel

---
+++ WebPlatfrom/Models/WebModels/AdModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.WebModels
+{
+    public class AdModel
+    {
+        public string Begin { get; set; }
+
+        public string End { get; set; }
+
+        public string Name { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
